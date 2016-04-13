c23e43337b42378229520036191f9984dd59fbfc
Stepan <despairandpain@gmail.com>
2016-04-14 00:10:19 +0500

    server + console output

--- app/app.py
+++ app/app.py
@@ -6,54 +6,78 @@ import re
 import time
 
 
-input_values = input('Введите город, улицу, номер дома через запятую: ')
-print(input_values)
-
-input_values = re.sub(' ', '', input_values)
-t_input_values = translit(input_values, 'ru', reversed=True)
-input_list = t_input_values.split(',')
-print(input_list)
-city = input_list[0]
-street = input_list[1]
-house_number = input_list[2]
-
-address = city+'/'+street+'/'+house_number
-q = address
-
-params = urllib.parse.urlencode({'format': 'json'})
-url = "http://nominatim.openstreetmap.org/search/{1}?{0}".format(params, q)
-print(url)
-req = urllib.request.Request(url, headers={'User-Agent': 'super'})
-_start_time = time.time()
-with urllib.request.urlopen(req) as res:
-    print("Время выполнения GET запроса: {:.3f} с".format(time.time() - _start_time))
-    j = json.loads(res.read().decode('utf-8'))
-    lat = j[0]['lat']
-    lon = j[0]['lon']
-    print(j)
+def main():
+    input_string_values = input('Введите город, улицу, номер дома через запятую: ')
+    test_data(input_string_values)
 
 
-params = urllib.parse.urlencode({'lat': lat, 'lon': lon, 'format': 'json'})
-url = "http://nominatim.openstreetmap.org/reverse?%s" % params
-print(url)
+def test_data(input_string):
+    # убрать пробелы и разбиты на состовляющие
+    input_data = re.sub(' ', '', input_string).split(',')
+    input_city = input_data[0]
+    input_street = input_data[1]
+    input_house_number = input_data[2]
 
-req = urllib.request.Request(url, headers={'User-Agent': 'super'})
-_start_time = time.time()
-with urllib.request.urlopen(req) as f:
-    print("Время выполнения GET запроса: {:.3f} с".format(time.time() - _start_time))
-    j = json.loads(f.read().decode('utf-8'))
-    address = j.get('address')
-    new_house_number = address['house_number']
-    new_street = address['road']
-    new_city = address['city']
+    input_values = re.sub(' ', '', input_string)
+    # транслитерация
+    t_input_values = translit(input_values, 'ru', reversed=True)
+    input_list = t_input_values.split(',')
 
-print('=====================')
-print('Адрес полученный:')
-print('Город: '+new_city)
-print('Улица: '+new_street)
-print('Номер дома: '+new_house_number)
-print('=====================')
-print('Адрес заданный:')
-print('Город: '+city)
-print('Улица: '+street)
-print('Номер дома: '+house_number)
+    city = input_list[0]
+    street = input_list[1]
+    house_number = input_list[2]
+
+    address = city+'/'+street+'/'+house_number
+    q = address
+
+    # get запрос к серверу для получения координат
+    params = urllib.parse.urlencode({'format': 'json'})
+    url = "http://nominatim.openstreetmap.org/search/{1}?{0}".format(params, q)
+    print(url)
+    req = urllib.request.Request(url, headers={'User-Agent': 'super'})
+    _start_time = time.time()
+    with urllib.request.urlopen(req) as res:
+        time_1 = time.time() - _start_time
+        print("Время выполнения GET запроса: {:.3f} с".format(time.time() - _start_time))
+        j = json.loads(res.read().decode('utf-8'))
+        try:
+            lat = j[0]['lat']
+            lon = j[0]['lon']
+        except IndexError:
+            print('Out of range')
+        except Exception:
+            print(Exception)
+
+    params = urllib.parse.urlencode({'lat': lat, 'lon': lon, 'format': 'json'})
+    url = "http://nominatim.openstreetmap.org/reverse?%s" % params
+    print(url)
+
+    # get запрос к сервену для поллучения адреса
+    req = urllib.request.Request(url, headers={'User-Agent': 'super'})
+    _start_time = time.time()
+    with urllib.request.urlopen(req) as f:
+        time_2 = time.time() - _start_time
+        print("Время выполнения GET запроса: {:.3f} с".format(time.time() - _start_time))
+        j = json.loads(f.read().decode('utf-8'))
+        address = j['address']
+        new_house_number = address['house_number']
+        new_street = address['road']
+        new_city = address['city']
+
+    print('=====================')
+    print('Адрес полученный:')
+    print('Город: '+new_city)
+    print('Улица: '+new_street)
+    print('Номер дома: '+new_house_number)
+    print('=====================')
+    print('Адрес заданный:')
+    print('Город: '+input_city)
+    print('Улица: '+input_street)
+    print('Номер дома: '+input_house_number)
+
+    return [new_city, new_street, new_house_number, input_city, input_street, input_house_number, round(time_1, 3),
+            round(time_2, 3)]
+
+
+if __name__ == "__main__":
+   main()
--- protei.py
+++ protei.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template
-import urllib.request
-import urllib.parse
+from flask import Flask, render_template, request, redirect
+from app import app as app_console
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', result=0)
+
+
+@app.route('/', methods=['POST'])
+def get_data():
+    output_list = app_console.test_data(request.form['city']+','+request.form['street']+','+request.form['house_number'])
+    return render_template('home.html', result=1, new_city=output_list[0], new_street=output_list[1],
+                    new_number=output_list[2], old_city=output_list[3], old_street=output_list[4],
+                    old_number=output_list[5], time_1=output_list[6], time_2=output_list[7])
 
 
 if __name__ == '__main__':
